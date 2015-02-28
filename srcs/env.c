2eba8724ab7d0ba53dab66b13e4a4e44400db684
gmp <georges@leaneous.com>
2015-02-28 13:16:22 +0100

    init

--- srcs/env.c
+++ srcs/env.c
@@ -6,13 +6,13 @@
 /*   By: gmp <gmp@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/28 13:11:43 by gmp               #+#    #+#             */
-/*   Updated: 2015/02/28 13:12:04 by gmp              ###   ########.fr       */
+/*   Updated: 2015/02/28 13:16:00 by gmp              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "base.h"
 
-t_env	*getEnv()
+t_env	*getEnv(void)
 {
 	static t_env	*e = NULL;
 	if (e == NULL){
