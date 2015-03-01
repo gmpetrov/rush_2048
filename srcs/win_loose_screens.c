1cc7aa978afe755f8028b262bf98f6348285de0e
gmp <georges@leaneous.com>
2015-03-01 18:06:56 +0100

    update norm

--- srcs/win_loose_screens.c
+++ srcs/win_loose_screens.c
@@ -6,7 +6,7 @@
 /*   By: gmp <gmp@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/01 16:04:58 by gmp               #+#    #+#             */
-/*   Updated: 2015/03/01 17:51:41 by gmp              ###   ########.fr       */
+/*   Updated: 2015/03/01 18:05:26 by gmp              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ void	loose_screen(void)
 
 	e = get_env();
 	clear();
-	print_loose((LINES / 2), (COLS / 2) - 40);
+	mvwprintw(stdscr, (LINES / 2), (COLS / 2) - 5, \
+		"You loose !");
 	mvwprintw(stdscr, (LINES / 2) - 5 + 12, \
 		(COLS / 2) - 10, "Your score : %d", e->score);
 	mvwprintw(stdscr, LINES - 2, 2, "%s", "Press escape to quit");
@@ -39,7 +40,8 @@ void	win_screen(void)
 
 	e = get_env();
 	clear();
-	print_win((LINES / 2), (COLS / 2) - 40);
+	mvwprintw(stdscr, (LINES / 2), (COLS / 2) - 5, \
+		"You win !");	
 	mvwprintw(stdscr, (LINES / 2) - 5 + 12, \
 		(COLS / 2) - 10, "Your score : %d", e->score);
 	mvwprintw(stdscr, LINES - 2, 2, "%s", "Press escape to quit");
@@ -50,48 +52,4 @@ void	win_screen(void)
 		if (c == 27)
 			quit();
 	}
-}
-
-void	print_win(int y, int x)
-{
-	mvwprintw(stdscr, y - 13, x, " __      __                         \
-	__       __  __                  __ ");
-	mvwprintw(stdscr, y - 12, x, "/* \\    /* |                       \
-	/* |  _  /* |/* |                /* |");
-	mvwprintw(stdscr, y - 11, x, "$$  \\  /*$/*_____   __    __      \
-	$$ | /*\\ $$ |$$/* _______        $$ |");
-	mvwprintw(stdscr, y - 10, x, " $$  \\/*$/**     \\ /* |  /* |    \
-	$$ |/*  \\$$ |/* |/*      \\       $$ |");
-	mvwprintw(stdscr, y - 9, x, "  $$  $$/**$$$$$  |$$ |  $$ |      \
-	$$ /*$$  $$ |$$ |$$$$$$$  |      $$ |");
-	mvwprintw(stdscr, y - 8, x, "   $$$$/*$$ |  $$ |$$ |  $$ |      \
-	$$ $$/*$ $$ |$$ |$$ |  $$ |      $$/*");
-	mvwprintw(stdscr, y - 7, x, "    $$ | $$ \\__$$ |$$ \\__$$ |    \
-	$$$$/* $$$$ |$$ |$$ |  $$ |       __ ");
-	mvwprintw(stdscr, y - 6, x, "    $$ | $$    $$/*$$    $$/*      \
-	$$$/*   $$$ |$$ |$$ |  $$ |      /* |");
-	mvwprintw(stdscr, y - 5, x, "    $$/*  $$$$$$/*  $$$$$$/        \
-	$$/      $$/ $$/ $$/   $$/       $$/ ");
-}
-
-void	print_loose(int y, int x)
-{
-	mvwprintw(stdscr, y - 5, x, " __      __                         \
-	__                                               ");
-	mvwprintw(stdscr, y - 4, x, "/  \\    /  |                      \
-	/* |                                              ");
-	mvwprintw(stdscr, y - 3, x, "$$  \\  /*$/*_____   __    __      \
-	$$ |        ______    ______    _______   ______  ");
-	mvwprintw(stdscr, y - 2, x, " $$  \\/*$/**     \\ /* |  /* |    \
-	$$ |       /*     \\  /*     \\  /*      | /*     \\ ");
-	mvwprintw(stdscr, y - 1, x, "  $$  $$/**$$$$$  |$$ |  $$ |      \
-	$$ |      /*$$$$$  |/*$$$$$  |/*$$$$$$/*/*$$$$$  |");
-	mvwprintw(stdscr, y - 0, x, "   $$$$/*$$ |  $$ |$$ |  $$ |      \
-	$$ |      $$ |  $$ |$$ |  $$ |$$      \\ $$    $$ |");
-	mvwprintw(stdscr, y + 1, x, "    $$ | $$ \\__$$ |$$ \\__$$ |    \
-	$$ |_____ $$ \\__$$ |$$ \\__$$ | $$$$$$  |$$$$$$$$/*");
-	mvwprintw(stdscr, y + 2, x, "    $$ | $$    $$/*$$    $$/*      \
-	$$       |$$    $$/*$$    $$/*/*    $$/*$$       |");
-	mvwprintw(stdscr, y + 3, x, "    $$/*  $$$$$$/*  $$$$$$/*       \
-	$$$$$$$$/* $$$$$$/*  $$$$$$/* $$$$$$$/*  $$$$$$$/*");
 }
