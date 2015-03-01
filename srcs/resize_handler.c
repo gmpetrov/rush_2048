fbb6bdc63b62abf9f426053a9b513390e4d8cd37
gmp <georges@leaneous.com>
2015-03-01 19:38:41 +0100

    resize handling for win and loose screen

--- srcs/resize_handler.c
+++ srcs/resize_handler.c
@@ -3,23 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   resize_handler.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdufaud <mdufaud@student.42.fr>            +#+  +:+       +#+        */
+/*   By: gmp <gmp@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/01 18:59:35 by mdufaud           #+#    #+#             */
-/*   Updated: 2015/03/01 19:20:21 by mdufaud          ###   ########.fr       */
+/*   Updated: 2015/03/01 19:37:58 by gmp              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "base.h"
+
+static void	draw_loose_screen(void)
+{
+	t_env	*e;
+
+	e = get_env();	
+	clear();
+	mvwprintw(stdscr, (LINES / 2), (COLS / 2) - 5, \
+		"You loose !");
+	mvwprintw(stdscr, (LINES / 2) - 5 + 12, \
+		(COLS / 2) - 10, "Your score : %d", e->score);
+	mvwprintw(stdscr, LINES - 2, 2, "%s", "Press escape to quit");
+	refresh();
+}
+
+static void	draw_win_screen(void)
+{
+	t_env	*e;
+
+	e = get_env();
+	clear();
+	mvwprintw(stdscr, (LINES / 2), (COLS / 2) - 5, "You win !");
+	mvwprintw(stdscr, (LINES / 2) - 5 + 12,\
+		(COLS / 2) - 10, "Your score : %d", e->score);
+	mvwprintw(stdscr, LINES - 2, 2, "%s", "Press escape to quit");
+	refresh();
+}
 
 static void	choose_menu(t_env *e)
 {
 	if (e->is_menu == 1)
 		draw_menu();
 	else if (e->is_menu == WIN_MENU)
-		win_screen();
+		draw_win_screen();
 	else if (e->is_menu == LOOSE_MENU)
-		loose_screen();
+		draw_loose_screen();
 	else if (e->is_menu == -42)
 	{
 		clear();
@@ -40,6 +67,5 @@ void		resize_handler(int sig)
 	e = get_env();
 	e->width = COLS;
 	e->height = LINES;
-	mvprintw(0, 0, "COLS = %d, LINES = %d", e->width, e->height);
 	choose_menu(e);
 }
